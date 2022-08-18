9397233203d251569e760bc3ba0f2cc8f8f49050
JayJay1024 <lygm1062@gmail.com>
2022-08-18 13:21:24 +0800

    Refactor fee market

--- subql/packages/apps/src/handler/logic/feemarket/types.ts
+++ subql/packages/apps/src/handler/logic/feemarket/types.ts
@@ -1,8 +1,55 @@
-import type { AccountId, Balance } from '@polkadot/types/interfaces';
-import type { Struct } from '@polkadot/types-codec';
+import type {
+  AccountId,
+  Balance,
+  BlockNumber,
+  LaneId,
+} from "@polkadot/types/interfaces";
+import type { Option, BTreeMap, u64 } from "@polkadot/types";
+import type { Struct } from "@polkadot/types-codec";
+import type { ITuple } from "@polkadot/types-codec/types";
+
+// The value is the specName
+export enum DarwiniaChain {
+  Crab = "Crab",
+  Darwinia = "Darwinia",
+  Pangolin = "Pangolin",
+  Pangoro = "Pangoro",
+  CrabParachain = "Crab Parachain",
+  DarwiniaParachain = "Darwinia Parachain",
+  PangolinParachain = "Pangolin Parachain",
+}
+
+export type FeeMarketApiSection =
+  | "feeMarket"
+  | "crabFeeMarket"
+  | "darwiniaFeeMarket"
+  | "pangolinFeeMarket"
+  | "pangoroFeeMarket"
+  | "crabParachainFeeMarket"
+  | "pangolinParachainFeeMarket";
+
+export type MessageNonce = u64;
 
 export interface PalletFeeMarketRelayer extends Struct {
   id: AccountId;
   collateral: Balance;
   fee: Balance;
+}
+
+export interface SlashReport {
+  lane: LaneId;
+  message: MessageNonce;
+  sentTime: BlockNumber;
+  confirmTime: Option<BlockNumber>;
+  delayTime: Option<BlockNumber>;
+  accountId: AccountId;
+  amount: Balance;
+}
+
+export interface RewardItem {
+  toSlotRelayer?: Option<ITuple<[AccountId, Balance]>>;
+  toAssignedRelayers?: BTreeMap<AccountId, Balance>;
+  toMessageRelayer: Option<ITuple<[AccountId, Balance]>>;
+  toConfirmRelayer: Option<ITuple<[AccountId, Balance]>>;
+  toTreasury: Option<Balance>;
 }
--- subql/packages/apps/src/handler/logic/staking/handlers.ts
+++ subql/packages/apps/src/handler/logic/staking/handlers.ts
@@ -3,22 +3,25 @@ import type { Balance, AccountId } from "@polkadot/types/interfaces";
 
 import { StakingStash, StakingRewarded } from "../../../types";
 
-export const handlerStakingRewarded = async (event: SubstrateEvent): Promise<void> => {
+export const handlerStakingRewarded = async (
+  event: SubstrateEvent
+): Promise<void> => {
   const {
     event: {
       data: [paramStash, paramAmount],
     },
   } = event;
 
+  const amount = (paramAmount as Balance).toBigInt();
+  const stashId = (paramStash as AccountId).toString();
+
   const blockTime = event.block.timestamp;
   const blockNumber = event.block.block.header.number.toNumber();
   const extrinsicIndex = event.extrinsic?.idx;
   const eventIndex = event.idx;
 
-  const amount = (paramAmount as Balance).toBigInt();
-  const stashId = (paramStash as AccountId).toString();
-
-  const stashRecord = (await StakingStash.get(stashId)) || new StakingStash(stashId);
+  const stashRecord =
+    (await StakingStash.get(stashId)) || new StakingStash(stashId);
   stashRecord.totalRewarded = (stashRecord.totalRewarded || BigInt(0)) + amount;
   await stashRecord.save();
 
