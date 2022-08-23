f0fb48ca6936f2da5eb1b2fa431de1576abbad1e
nada <37610161+furoxr@users.noreply.github.com>
2022-08-23 19:10:51 +0800

    Add ethv2 thegraph for pangoro

---
+++ thegraph/ethv2/src/outbound.ts
@@ -0,0 +1,33 @@
+import { Bytes } from "@graphprotocol/graph-ts"
+import {
+  MessageAccepted, MessagesDelivered
+} from "../generated/outbound/outbound"
+import { MessageAcceptedEntity, MessagesDeliveredEntity } from "../generated/schema"
+
+export function handleMessageAccepted(event: MessageAccepted): void {
+  let id = Bytes.fromHexString(event.block.number.toHexString())
+  let message_accpeted = MessageAcceptedEntity.load(id)
+  if (!message_accpeted) {
+    message_accpeted = new MessageAcceptedEntity(id)
+  }
+  message_accpeted.source = event.params.source
+  message_accpeted.target = event.params.target
+  message_accpeted.nonce = event.params.nonce
+  message_accpeted.encoded = event.params.encoded
+  message_accpeted.block_number = event.block.number
+  message_accpeted.save()
+}
+
+export function handleMessagesDelivered(event: MessagesDelivered): void {
+  let id = Bytes.fromHexString(event.block.number.toHexString())
+  let message_delivered = MessagesDeliveredEntity.load(id)
+  if (!message_delivered) {
+    message_delivered = new MessagesDeliveredEntity(id)
+  }
+  message_delivered.begin = event.params.begin
+  message_delivered.end = event.params.end
+  message_delivered.results = event.params.results
+  message_delivered.block_number = event.block.number
+  message_delivered.save()
+}
+
