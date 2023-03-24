f50c9a17956bf7c9fa68399eb8b7ea92d9eaaa1d
nada <37610161+furoxr@users.noreply.github.com>
2023-03-24 09:33:34 +0800

    feat:index for pangolin-goerli

--- subql/packages/bridger/src/handler/chain/impls.ts
+++ subql/packages/bridger/src/handler/chain/impls.ts
@@ -71,6 +71,7 @@ export class PangolinHandler extends BasicChainHandler {
     return [
       new GenericBlockHandler(Chain.Pangolin),
       new BridgeS2SHandler(Chain.Pangolin),
+      new BridgeEthV2Handler(Chain.Pangolin),
     ];
   }
 }
