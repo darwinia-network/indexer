f9f9d5d5bc771b41ac37f7171a6bb1992918091d
fewensa <osuni@protonmail.com>
2023-04-25 16:52:46 +0800

    Code clean

--- subql/packages/bridger/src/handler/logic/bridge-s2s/chain/crab.ts
+++ subql/packages/bridger/src/handler/logic/bridge-s2s/chain/crab.ts
@@ -16,24 +16,11 @@ export class ChainCrabEventHandler implements BridgeS2SEventHandler {
         }).store();
         return;
       }
-      case 'bridgeCrabParachainMessages:MessageAccepted': {
-        await new NeedRelayBlockStorage(event, BridgeS2SRelayBlockOrigin.BridgeCrabParachain, {
-          onDemandType: BridgeS2SOnDemandType.SendMessage,
-        }).store();
-        return;
-      }
     }
 
     // dispatch
     if (section === 'bridgeDarwiniaDispatch') {
       await new NeedRelayBlockStorage(event, BridgeS2SRelayBlockOrigin.BridgeDarwinia, {
-        onDemandType: BridgeS2SOnDemandType.Dispatch,
-        additional: method,
-      }).store();
-      return;
-    }
-    if (section === 'bridgeCrabParachainDispatch') {
-      await new NeedRelayBlockStorage(event, BridgeS2SRelayBlockOrigin.BridgeCrabParachain, {
         onDemandType: BridgeS2SOnDemandType.Dispatch,
         additional: method,
       }).store();
