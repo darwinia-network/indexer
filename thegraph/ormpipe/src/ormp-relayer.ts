29f4ba169437c4b95ddd96a81b3512e41e591b80
fewensa <osuni@protonmail.com>
2023-08-15 02:58:50 +0000

    update schema

--- thegraph/ormpipe/src/ormp-relayer.ts
+++ thegraph/ormpipe/src/ormp-relayer.ts
@@ -5,14 +5,12 @@ import {
   SetDstPrice as SetDstPriceEvent
 } from "../generated/OrmpRelayer/OrmpRelayer"
 import {
-  Assigned,
-  SetApproved,
-  SetDstConfig,
-  SetDstPrice
+  OrmpRelayerAssigned,
+  OrmpRelayerSetDstPrice
 } from "../generated/schema"
 
 export function handleAssigned(event: AssignedEvent): void {
-  let entity = new Assigned(
+  let entity = new OrmpRelayerAssigned(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.msgHash = event.params.msgHash
@@ -26,37 +24,8 @@ export function handleAssigned(event: AssignedEvent): void {
   entity.save()
 }
 
-export function handleSetApproved(event: SetApprovedEvent): void {
-  let entity = new SetApproved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.approve = event.params.approve
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetDstConfig(event: SetDstConfigEvent): void {
-  let entity = new SetDstConfig(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.chainId = event.params.chainId
-  entity.baseGas = event.params.baseGas
-  entity.gasPerByte = event.params.gasPerByte
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
 export function handleSetDstPrice(event: SetDstPriceEvent): void {
-  let entity = new SetDstPrice(
+  let entity = new OrmpRelayerSetDstPrice(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.chainId = event.params.chainId
