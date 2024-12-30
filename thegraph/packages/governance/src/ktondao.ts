3c1dec39f223826a9b6e7960bce4c837b42f55f2
fewensa <37804932+fewensa@users.noreply.github.com>
2024-12-30 16:26:28 +0800

    Code clean and add governance project (#155)
    * governance
    
    * CI
    
    * script
    
    * fix types and compile
    
    * clean indexerHints
    
    * Add dcdao and ringdao

---
+++ thegraph/packages/governance/src/ktondao.ts
@@ -0,0 +1,28 @@
+import { ProposalCreated as ProposalCreatedEvent } from "../generated/Ktondao/Ktondao";
+import { ProposalCreated } from "../generated/schema";
+
+export function handleProposalCreated(event: ProposalCreatedEvent): void {
+  let entity = new ProposalCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32()).toHexString()
+  );
+  entity.origin = "KtonDAO";
+  entity.proposalId = event.params.proposalId;
+  entity.proposer = event.params.proposer.toHexString();
+  entity.targets = event.params.targets.map<string>(value =>
+    value.toHexString()
+  );
+  entity.values = event.params.values;
+  entity.signatures = event.params.signatures;
+  entity.calldatas = event.params.calldatas.map<string>(value =>
+    value.toHexString()
+  );
+  entity.voteStart = event.params.voteStart;
+  entity.voteEnd = event.params.voteEnd;
+  entity.description = event.params.description;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.save();
+}
